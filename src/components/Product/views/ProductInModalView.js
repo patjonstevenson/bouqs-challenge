d4d9503adc4e12629f177da8fee86e57599a004d
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-08-07 15:05:44 -0700

    images rendering in modal, with different images for each product

--- src/components/Product/views/ProductInModalView.js
+++ src/components/Product/views/ProductInModalView.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithHeaders as axios } from "../../../requests";
 
 export default ({ data }) => {
     console.log("In ProductInModalView! data: ", data);
-    const variantName = data.variant ? data.variant.name : null;
-    const image = data.images.find(i => i.name === variantName);
+    const variantName = data.variant.name || null;
+    const imageURL = data.images ? data.images[0].url : null// data.images.find(i => i.option === variantName) || null;
+    // So that we get different images from the same image generator link
+    const src = imageURL ? `${imageURL.slice(0, imageURL.length)}?t=${Date.now()}` : null;
+    console.log("src in ProductInModalView: ", src);
+
     return (
         <div key={data.id} className="product-in-modal">
-            <img src={data.images.url} alt={data.image_alt_tags} />
+            {imageURL ? <img src={src} alt={data.image_alt_tags} /> : "LOADING IMAGE"}
             <div className="product-info-modal">
                 <h2>{data.name}</h2>
-                <p className="product-price">${Math.round(data.price)}</p>
+                <p className="product-price">${Math.round(data.variant.prices.regular)}</p>
                 <button onClick={() => { console.log("Added to cart."); }}>Add to Cart</button>
                 <h4>Description</h4>
                 <p className="product-description">{data.description}</p>
