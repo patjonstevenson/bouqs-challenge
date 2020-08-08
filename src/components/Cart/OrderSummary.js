900047adb19649e5c5b5541e300e8026b514b1d4
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-08-08 15:47:07 -0700

    wrote OrderSummary

---
+++ src/components/Cart/OrderSummary.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default cart => {
+    console.log("Cart in OrderSummary: ", cart);
+    const productIds = Object.keys(cart);
+    const subtotal = productIds.reduce((sum, curr) =>
+        sum + curr.variant.prices.regular
+    );
+    return (
+        <div className="order-summary">
+            <span>SubTotal:</span><span>{subtotal}</span>
+        </div>
+    );
+}
